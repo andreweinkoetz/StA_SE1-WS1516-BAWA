bf09028172b983146460dd59f98d4a47e9a313e1
hm-aweink <andre.wenkoetz@hm.edu>
2015-10-15 13:34:25 +0200

    Initial commit

---
+++ web/Beispiele/user-read.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace web.beispiele {
+    
+    
+    public partial class user_read {
+        
+        /// <summary>
+        /// form1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// grdUsers-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdUsers;
+        
+        /// <summary>
+        /// odsUsers-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource odsUsers;
+        
+        /// <summary>
+        /// HyperLink1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+    }
+}
---
+++ web/default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="web._default" Culture="de-DE" %>
+
+<!DOCTYPE html>
+
+<html lang="de-DE">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
+    <meta charset = "ISO-8859-1" />
+    <title>Pizza Pizza!</title>
+    </head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <h1>Willkommen bei Heigert's Pizza!</h1></div>
+    <p>
+        <a href="Beispiele/default.aspx">Hier</a> gehts weiter zu einigen Beispielprogrammen ...
+    </p> 
+        <p><i>Version: 1.10.2015</i></p>
+    </form>
+</body>
+</html>
