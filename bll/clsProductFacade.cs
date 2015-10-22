27830816373943597839975d0b6f3ca5638acd89
hm-aweink <andre.wenkoetz@hm.edu>
2015-10-22 15:01:19 +0200

    added Product & Facade classes

---
+++ bll/clsProductFacade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bll
+{
+    class clsProductFacade
+    {
+        clsProductCollection _productCol; //Objektvariable für Product-Collection,
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public clsProductFacade()
+        {   // instatiierung _productCol
+            _productCol = new clsProductCollection();
+        }
+
+        /// <summary>
+        /// Alle Produkte lesen
+        /// </summary>
+        public List<clsProduct> ProductsGetAll()
+        {
+            return _productCol.getAllProduct();
+        } //ProductsGetAll()
+    } // clsProductFacade
+}
