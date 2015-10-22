6193b5a4811446ce1a59d0683fb4a61467eb0b78
hm-aweink <andre.wenkoetz@hm.edu>
2015-10-22 14:53:15 +0200

    clsProductCollection added

---
+++ bll/clsProductCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bll
+{
+    class clsProductCollection : clsBLLCollections
+    {
+        string _databaseFile; // String zur Access-Datei
+        DAL.DALObjects.dDataProvider _myDAL; // DAL: Zugriff auf die Datenbank
+
+        internal clsProductCollection()
+        {
+            _databaseFile = System.Configuration.ConfigurationManager.AppSettings["AccessFileName"];
+            _myDAL = DAL.DataFactory.GetAccessDBProvider(_databaseFile);
+        }
+
+        internal List<clsProduct> getAllProducts()
+        {
+            DataSet _myDataSet = _myDAL.GetStoredProcedureDSResult("QPGetAllProducts");
+            DataTable _myDataTable = _myDataSet.Tables[0];
+            List<clsPoduct> _myProductList = new List<clsPoduct>();
+
+            foreach (DataRow _dr in _myDataTable.Rows)
+            {
+                clsProduct _myProduct = DatarowToClsProduct(_dr);
+                _myProductList.Add(_myProduct);
+            }
+
+            return _myProductList;
+        }
+
+        internal clsProductCollection DatarowToClsProduct(DataRow _dr)
+        {
+            clsProductCollection _myProduct = new clsProduct();
+            _myProduct.Id = AddIntFieldValue(_dr, "PID");
+            _myProduct.Name = AddStringFieldValue(_dr, "PName");
+            _myProduct.PricePerUnit = AddDoubleFieldValue(_dr, "PPricePerUnit");
+        }
+    }
+}
